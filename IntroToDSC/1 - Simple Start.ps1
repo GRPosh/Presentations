52f9454ae59a624f7c04ad9a9c4cfa418ed267c8
Micah Rairdon <Micah.Rairdon@Haworth.com>
2017-02-02 10:20:26 -0500

    Introduction to DSC - 2/1/2017

---
+++ IntroToDSC/1 - Simple Start.ps1
@@ -0,0 +1,21 @@
+﻿configuration New-MyFirstConfiguration
+{
+    param([string[]]$Nodes)
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node $Nodes
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        File Telnet
+        {
+           Ensure = "Present"
+           Type = "Directory"
+           DestinationPath = "C:\My"
+        }
+      
+    }
+}
+
+New-MyFirstConfiguration -Nodes "localhost"
+Start-DscConfiguration -Path .\New-MyFirstConfiguration -Wait -Verbose -Force
---
+++ IntroToDSC/2 - Installing Software with Chocolatey.ps1
@@ -0,0 +1,25 @@
+﻿Install-Module cChoco
+
+configuration New-MySecondConfiguration
+{
+    param([string[]]$Nodes)
+
+    Import-DscResource -Module cChoco
+
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node $Nodes
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        cChocoPackageInstaller Chrome
+        {
+           Ensure = "present"
+           Name = "googlechrome"
+        }
+      
+    }
+}
+
+New-MySecondConfiguration -Nodes "localhost"
+Start-DscConfiguration -Path .\New-MySecondConfiguration -Wait -Verbose -Force
