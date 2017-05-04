45af3d587c5b89e4ed04e7d795914e986bdbd218
Micah Rairdon <Micah.rairdon@gmail.com>
2017-05-04 19:37:40 -0400

    PSWebServer Demo - 5/3/2017

---
+++ PSWebServer/PSWebServerExamples.ps1
@@ -0,0 +1,76 @@
+﻿#PSWebServer - gist: https://gist.github.com/Tiberriver256/868226421866ccebd2310f1073dd1a1e
+
+#Example 1
+#---------------------
+New-PSWebServer
+
+
+#Example 2
+#---------------------
+New-PSWebServer -Public
+
+
+#Example 3
+#---------------------
+New-PSWebServer -url http://localhost:8000/ -webschema @(
+    Add-GetHandler -Path '/' -Script { "Welcome to PSWebServer!" }
+    Get '/process'       { get-process |select name, id, path | ConvertTo-Json }
+    Get '/prettyprocess' { Get-Process |ConvertTo-HTML name, id, path }
+ )
+
+
+#Example 4
+#--------------------- 
+New-PSWebServer -url http://localhost:8000/ -webschema @(
+    Get '/' 
+    Post '/startprocessbypost' {
+       $processname = $Body.NotePad
+       Start-Process $processname -PassThru | ConvertTo-HTML
+    } 
+    DELETE '/startprocessbyparameter/{FirstName}/{LastName}' { "First Name: $($Parameters.FirstName)  Last Name: $($Parameters.LastName)" }
+ )
+ 
+Invoke-RestMethod -Uri http://localhost:8000/startprocessbyparameter/Bob/Dylan
+Invoke-RestMethod -Uri http://localhost:8000/startprocessbypost -Method Post -Body @{NotePad ="Notepad"}
+
+
+#Example 5 - Use ShowMeNames.HTML to send data from a form to this URL
+#--------------------- 
+New-PSWebServer -url http://localhost:8000/ -webschema @(
+    Post '/ShowMeNames' {
+       $Body | ConvertTo-Html | Out-String
+    }
+ )
+
+
+#Example 6
+#---------------------
+New-PSWebServer -url 'http://localhost:8080/' -webschema @(
+    Post '/startprocessbypost' {
+       $processname = $Body.ProcessName
+       Start-Process $processname
+    } @("Administrators")
+    Get '/Process' { Get-Process } @("Users","Administrators")
+ ) -AuthenticationScheme "Negotiate"
+
+
+#Example 7
+#---------------------
+New-PSWebServer -url "http://$($env:COMPUTERNAME):8001/" -webschema @(
+    Get '/MoveOU/{ComputerName}/' {
+            "Moving $($parameters.ComputerName) from $($ADComputer.DistinguishedName) to 'OU=Clients,DC=MyDomain,DC=com'" >> D:\MDTConfiguration\MoveOU.log
+            $ADComputer | Move-ADObject -TargetPath  "OU=Clients,DC=MyDomain,DC=com"
+        }
+) -Public
+
+
+<#
+
+    Some ideas for the future
+
+    1. HTTPS Support
+    2. Extensibility / Plug-in Support system
+    3. Better debugging / development experience
+    4. Impersonation
+
+#>
---
+++ PSWebServer/PowerShellWebPrompt.ps1
@@ -0,0 +1,66 @@
+﻿$WebSchema = @()
+$WebSchema += Add-GetHandler -Path '/' -Script {
+@"
+    <!doctype html>
+    <html>
+        <head>
+            <script src="https://cdn.jsdelivr.net/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/1.2.0/js/jquery.terminal.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/1.2.0/css/jquery.terminal.min.css" rel="stylesheet"/>
+            <style type="text/css">
+                html, body {
+                    height: 100%;
+                    margin: 0;
+                }
+
+                #term_demo {
+                    min-height: 100%; 
+                }
+            </style>
+        </head>
+        <body>
+            <div id="term_demo"></div>
+            <script>
+                jQuery(function(`$, undefined) {
+                    `$('#term_demo').terminal(function(command, term) {
+                        if(command == 'cls') {
+                            term.clear();
+                        } else {
+                            return $.post('command', {command: command})
+                                .fail(function(xhr,status,error){
+                                    term.error(xhr.responseText);
+                                    term.resume();
+                                });
+                        }
+                    }, {
+                        greetings: 'PowerShell - WebShell',
+                        name: 'ps_demo',
+                        prompt: function(callback) {
+                            var Response = $.ajax({
+                                type: "POST",
+                                url: "Prompt",
+                                async: false
+                            }).responseText;
+
+                            callback(Response);
+                        }
+                    });
+                });
+            </script>
+        </body>
+    </html>
+"@
+}
+$WebSchema += Add-PostHandler -Path '/Command' -Script {
+    $null = New-Event -SourceIdentifier ConsoleMessageEvents -MessageData ($Body)
+    $null = New-Event -SourceIdentifier ConsoleMessageEvents -MessageData ($Request | ConvertTo-Json | Out-String)
+    $Result = & ([scriptblock]::Create($Body.command))
+    $Result | Out-String
+}
+$WebSchema += Add-PostHandler -Path '/Prompt' -Script {
+    prompt
+}
+
+New-PSWebServer -url "http://localhost:8080/" -webschema $WebSchema
+#New-PSWebServer -url "http://+:8080/" -webschema $WebSchema -Public
